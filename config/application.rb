2090dfc2cc037309052d75c45cdba43bacef7d59
Nalin Pawar <nalinpawar@Nalins-MacBook-Pro.local>
2020-06-28 16:46:09 +0530

    bumped rails-version/added apland-theme

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,5 @@ module Wakariera
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.assets.paths << Rails.root.join("app", "assets", "images", "client","portfolio","team")
-    config.assets.initialize_on_precompile = false
   end
 end
